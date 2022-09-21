75ff38192c6568564d931870b8f316e26b3322d2
Robert Taylor <rvtaylor94@gmail.com>
2022-09-20 18:38:18 -0600

    Time: 113 ms (10.37%), Space: 44.5 MB (5.28%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let words = s.split(' ');
+    console.log(words);
+
+    let idx = words.length - 1;
+    while (idx >= 0) {
+        if (words[idx].length > 0) {
+            return words[idx].length;
+        }
+        idx -= 1;
+    }
+};
