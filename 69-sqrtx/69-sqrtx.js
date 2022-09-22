23bef9f87a9c56e445bba33028f36148686414ca
Robert Taylor <rvtaylor94@gmail.com>
2022-09-22 11:10:47 -0600

    Time: 190 ms (10.43%), Space: 43 MB (88.94%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    
+    for (let i = 0; i <= x; i++) {
+        if (i * i === x) {
+            return i;
+        }
+        if (i * i > x) {
+            return i - 1;
+        }
+    } 
+
+    return 0;
+};
